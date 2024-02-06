9d7ad6fe8eb8c10e004d5f5c3b8dadfacc8146f7
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-02-06 12:21:39 +0000

    [Add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/show'
   get 'books/edit'
+  
+  post 'books' => 'books#create'
 
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
