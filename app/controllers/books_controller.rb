95bdc4114a355cb342b67de4adabba75c8995cb0
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-02-07 08:21:03 +0000

    [Add]Flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -37,6 +38,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
